3be0e0b0385058c61708a13aedbbb4b00e2c825a
Rakibul Islam <boraborhasib@gmail.com>
2024-04-06 22:30:21 +0600

    s

---
+++ grocery.py
@@ -0,0 +1,7 @@
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+
+myGrocery = ['🥚 Eggs','🥑 Avocados','🍪 Cookies','🌶 Hot Pepper Jam','🫐 Blueberries','🥦 Broccoli']
+
+for item in myGrocery:
+    print(item)
